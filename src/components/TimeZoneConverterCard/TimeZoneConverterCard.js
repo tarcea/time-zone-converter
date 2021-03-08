d726feb878a199c4bf894aec5e8981c8236d9e8d
Filip Alempijevik <55798305+alempijevik@users.noreply.github.com>
2021-03-09 00:01:11 +0100

    calendar added, styling and more elements

--- src/components/TimeZoneConverterCard/TimeZoneConverterCard.js
+++ src/components/TimeZoneConverterCard/TimeZoneConverterCard.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './TimeZoneConverterCard.module.css';
 import Clock from "../Clock/Clock";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+
 
 export default function TimeZoneConverterCard() {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <div className={styles.CardBackground}>
             <div className={styles.CardHeader}>
                 <p className={styles.TitleText}>TIME ZONE CONVERTER</p>
                 <Clock />
             </div>
-            <div className={styles.CurrentTimeZone}></div>
-            <div className={styles.MeetingTimeZone}></div>
-            <div className={styles.CardFooter}></div>
+            <div className="form">
+                <div className={styles.CurrentTimeZone}>
+                    <div className="formGroup">
+                        <div className="leftInput">
+                            <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                        </div>
+                        <div className="middleInput"></div>
+                        <div className="rightInput">
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.MeetingTimeZone}>
+                <div className="formGroup">
+                        <div className="leftInput">
+                            <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                        </div>
+                        <div className="middleInput"></div>
+                        <div className="rightInput">
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.CardFooter}></div>
+            </div>
         </div>
     )
 }
