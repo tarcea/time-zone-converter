c94a411bc9d5c21535a3611f22c9b70ab04e0bde
Natalie Whitton <newhitton@gmail.com>
2021-03-09 14:19:29 +0100

    cleaned code

--- src/components/TimeZoneConverterCard/TimeZoneConverterCard.js
+++ src/components/TimeZoneConverterCard/TimeZoneConverterCard.js
@@ -5,7 +5,6 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
-import './ReactDatePickerOverride.css'
 
 
 export default function TimeZoneConverterCard() {
