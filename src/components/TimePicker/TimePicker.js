f2ea16dc008f0d8610fd15af2cbe389ea2d8b0d1
Filip Alempijevik <55798305+alempijevik@users.noreply.github.com>
2021-03-15 20:38:27 +0100

    selected dropdown bugfix
    dropdown selected element fix

--- src/components/TimePicker/TimePicker.js
+++ src/components/TimePicker/TimePicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Combobox, ComboboxInput, ComboboxPopover, ComboboxList
 } from "@reach/combobox"
@@ -7,12 +7,14 @@ import {
 export default function TimePicker(props) {
 
     const {dropDownTimes, defaultValue} = props
+    const [homeTime, setHomeTime] = useState()
 
     return (
         <div>
             <Combobox aria-labelledby="demo" openOnFocus={true}>
                 <ComboboxInput
-                    value={defaultValue}
+                    onChange={setHomeTime}
+                    value={homeTime}
                     style={{color: "white", backgroundColor:"rgba(204, 71, 71, 0.9)", fontSize: "17px", fontWeight: "700", width: "80px", border:"none", background:"transparent",}}
                      />
                 <ComboboxPopover>
